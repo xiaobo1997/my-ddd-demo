6c11ab5ab699dfe7ef86e321559f13dae42bb5db
xiaobo1997 <452387280@qq.com>
2026-05-15 11:51:46 +0800

    feat: 引入 Spring Boot 2.7.18 并创建启动类

---
+++ my-ddd-demo-start/src/main/java/com/viw/ddd/demo/DddDemoApplication.java
@@ -0,0 +1,15 @@
+package com.viw.ddd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xhb
+ */
+@SpringBootApplication
+public class DddDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddDemoApplication.class, args);
+    }
+}
