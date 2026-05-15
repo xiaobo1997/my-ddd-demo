69039f061853011fab989a1c1737cfd7e9042918
xiaobo1997 <452387280@qq.com>
2026-05-15 11:56:23 +0800

    feat: 补充 SubmitApplyOrderCommand 字段（发票抬头/金额/明细等）

--- my-ddd-demo-api/src/main/java/com/viw/ddd/demo/api/applyOrder/dto/SubmitApplyOrderCommand.java
+++ my-ddd-demo-api/src/main/java/com/viw/ddd/demo/api/applyOrder/dto/SubmitApplyOrderCommand.java
@@ -1,17 +1,41 @@
 package com.viw.ddd.demo.api.applyOrder.dto;
 
+import com.viw.ddd.demo.domain.applyOrder.vo.ApplyOrderDetailVO;
+import com.viw.ddd.demo.domain.applyOrder.vo.ApplyOrderExpressVO;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author xhb
  * @Date 2026/1/8
- * @Description :
  */
-
 @Data
 public class SubmitApplyOrderCommand implements Serializable {
 
+    /** 客户公司ID */
     private Long companyId;
+
+    /** 发票抬头 */
+    private String invoiceHeader;
+
+    /** 申请事由 */
+    private String subject;
+
+    /** 申请金额 */
+    private BigDecimal applyAmount;
+
+    /** 运费 */
+    private BigDecimal freightFee;
+
+    /** 服务费 */
+    private BigDecimal serviceFee;
+
+    /** 申请单明细列表 */
+    private List<ApplyOrderDetailVO> applyOrderDetailVOList;
+
+    /** 快递信息 */
+    private ApplyOrderExpressVO applyOrderExpressVO;
 }
